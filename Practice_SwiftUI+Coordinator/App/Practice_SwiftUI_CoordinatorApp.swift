4279dae0a17d5dc9e307e044e9e5ea417d085016
Jiho <yjh7827@naver.com>
2024-05-09 02:53:01 +0900

    feat: initial commit

---
+++ Practice_SwiftUI+Coordinator/App/Practice_SwiftUI_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_SwiftUI_CoordinatorApp.swift
+//  Practice_SwiftUI+Coordinator
+//
+//  Created by 유지호 on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice_SwiftUI_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Practice_SwiftUI+Coordinator/Presentation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice_SwiftUI+Coordinator
+//
+//  Created by 유지호 on 5/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
